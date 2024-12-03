16cb053b7402879f7cbe1706f22ba7e5f9266bfe
Otavio F <otaviofbrito@gmail.com>
2024-12-03 20:35:37 -0300

    fix path

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,126 +1,176 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import glob
-import plotly.io as pio
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Configurar título e descrição
-st.title("Análise de Dados de Eleições")
-st.write("Este dashboard apresenta gráficos dos dados de eleições brasileiras de 2024 processados.")
-
-# Carregar Dados
+# Configuração da função de carregamento de dados
 
 
 @st.cache_data
 def load_data():
-    # Substitua pelo caminho para o diretório onde o arquivo está localizado
-    # folder_path = "./Volumes/gold/tse/consulta_cand" ## -- Usar essa se for rodar sem kubernetes
+    # folder_path = "./Volumes/gold/tse/consulta_cand"
     folder_path = "/data/gold/tse/consulta_cand"
-
-    # Encontra o único arquivo CSV na pasta
     file_path = glob.glob(f"{folder_path}/part-*.csv")
 
-    # Verifica se algum arquivo foi encontrado
     if not file_path:
         raise FileNotFoundError(
-            f"Nenhum arquivo CSV encontrado na pasta especificada. {file_path}")
+            f"Nenhum arquivo CSV encontrado na pasta especificada: {file_path}"
+        )
 
-    # Como esperamos apenas um arquivo, selecionamos o primeiro
     file_path = file_path[0]
-
-    # Carrega o arquivo CSV no DataFrame
     return pd.read_csv(file_path)
 
 
-page = st.sidebar.selectbox('Página:', ['Candidatos', 'Bens declarados'])
+# Navegação entre páginas
+page = st.sidebar.selectbox(
+    'Página:', ['Candidatos', 'Grau de Instrução', 'Bens Declarados'])
 
+# Carregar os dados
 data = load_data()
 
+if page == 'Candidatos':
+    # Página: Candidatos
+    st.title("Análise de Dados de Candidatos - 2024")
+    st.write(
+        "Este dashboard apresenta gráficos dos dados de eleições brasileiras de 2024 processados.")
 
-# SECTION 1
-# DIV 1
-if st.checkbox("Mostrar dados brutos"):
-    st.write(data.head())
+    # Mostrar dados brutos
+    if st.checkbox("Mostrar dados brutos"):
+        st.write(data.head())
 
-columns = st.multiselect(
-    "Selecione colunas para análise", data.columns.tolist())
-if columns:
-    st.write(data[columns])
+    # Seleção de colunas
+    columns = st.multiselect(
+        "Selecione colunas para análise", data.columns.tolist())
+    if columns:
+        st.write(data[columns])
 
-# SECTION 2
-st.subheader("Gráficos Interativos")
-# DIV 1
-st.subheader("1. Distribuição de Candidatos por Estado e Gênero")
-st.write("Gráfico de barras empilhadas mostrando o número de candidatos por estado, divididos por Gênero.")
-grouped_data = data.groupby(
-    ["SG_UF", "DS_GENERO"]).size().reset_index(name="NUM_CANDIDATOS")
+    # Gráfico: Distribuição por Estado e Gênero
+    st.subheader("1. Distribuição de Candidatos por Estado e Gênero")
+    grouped_data = data.groupby(
+        ["SG_UF", "DS_GENERO"]).size().reset_index(name="NUM_CANDIDATOS")
+    fig = px.bar(
+        grouped_data,
+        x="SG_UF",
+        y="NUM_CANDIDATOS",
+        color="DS_GENERO",
+        title="Distribuição de Candidatos por Estado e Gênero",
+        labels={"NUM_CANDIDATOS": "Nº de Candidatos",
+                "SG_UF": "Estado", "DS_GENERO": "Gênero"},
+        barmode="stack",
+    )
+    st.plotly_chart(fig)
 
-fig = px.bar(
-    grouped_data,
-    x="SG_UF",
-    y="NUM_CANDIDATOS",
-    color="DS_GENERO",
-    title="Distribuição de Candidatos por Estado e Gênero",
-    labels={"NUM_CANDIDATOS": "Nº de Candidatos",
-            "SG_UF": "Estado", "DS_GENERO": "Gênero"},
-    barmode="stack",  # Configuração para barras empilhadas
-    color_discrete_map={"MASCULINO": "blue",
-                        "FEMININO": "pink"}  # Personalização de cores
-)
-st.plotly_chart(fig)
+    # Gráfico: Distribuição por Estado e Raça
+    st.subheader("2. Distribuição de Candidatos por Estado e Raça")
+    grouped_data = data.groupby(
+        ["SG_UF", "DS_COR_RACA"]).size().reset_index(name="NUM_CANDIDATOS")
+    fig = px.bar(
+        grouped_data,
+        x="SG_UF",
+        y="NUM_CANDIDATOS",
+        color="DS_COR_RACA",
+        title="Distribuição de Candidatos por Estado e Raça",
+        labels={"NUM_CANDIDATOS": "Nº de Candidatos",
+                "SG_UF": "Estado", "DS_COR_RACA": "Raça"},
+        barmode="stack",
+    )
+    st.plotly_chart(fig)
 
-# DIV 2
-st.subheader("2. Distribuição de Candidatos por Estado e Raça")
-st.write("Gráfico de barras empilhadas mostrando o número de candidatos por estado, divididos por Raça.")
-grouped_data = data.groupby(
-    ["SG_UF", "DS_COR_RACA"]).size().reset_index(name="NUM_CANDIDATOS")
+elif page == 'Grau de Instrução':
+    # Página: Grau de Instrução
+    st.title("Análise do Grau de Instrução")
+    st.write("Visualize os dados sobre  grau de instrução dos candidatos")
 
-fig = px.bar(
-    grouped_data,
-    x="SG_UF",
-    y="NUM_CANDIDATOS",
-    color="DS_COR_RACA",
-    title="Distribuição de Candidatos por Estado e Gênero",
-    labels={"NUM_CANDIDATOS": "Nº de Candidatos",
-            "SG_UF": "Estado", "DS_GENERO": "Gênero"},
-    barmode="stack"  # Personalização de cores
-)
-st.plotly_chart(fig)
+    # Seleção por estado
+    selected_states = st.multiselect(
+        "Selecione os estados para análise:",
+        options=data["SG_UF"].unique()
+    )
 
-# Título da aplicação
-st.title("Gráfico de Pizza Subdividido por Grau de Instrução")
+    if selected_states:
+        filtered_df = data[data["SG_UF"].isin(selected_states)]
+        grouped_df = (
+            filtered_df.groupby(["DS_GRAU_INSTRUCAO"], as_index=False)
+            .agg({"SQ_CANDIDATO": "count"})
+        )
+        grouped_df["Porcentagem"] = (
+            grouped_df["SQ_CANDIDATO"] / grouped_df["SQ_CANDIDATO"].sum()) * 100
 
-# Seleção dos estados
-selected_states = st.multiselect(
-    "Selecione os estados para incluir no gráfico:",
-    options=data["SG_UF"].unique()
-)
+        # Gráfico de pizza
+        fig = px.pie(
+            grouped_df,
+            names="DS_GRAU_INSTRUCAO",
+            values="Porcentagem",
+            title=f"Distribuição de Grau de Instrução - {
+                ', '.join(selected_states)}",
+        )
+        st.plotly_chart(fig)
+    else:
+        st.write("Selecione ao menos um estado para visualizar os gráficos.")
 
-# Filtrar os dados com base nos estados selecionados
-filtered_df = data[data["SG_UF"].isin(selected_states)]
+elif page == 'Bens Declarados':
+    # Página: Candidatos
+    data['VR_BEM_TOTAL'] = data['VR_BEM_TOTAL'].astype(
+        float)  # Certificar que é numérico
+    columns_to_keep = ['SG_UF', 'DS_GENERO', 'SG_PARTIDO', 'VR_BEM_TOTAL']
+    filtered_data = data[columns_to_keep]
 
-# Agrupar os dados por grau de instrução e calcular o total de candidatos
-grouped_df = (
-    filtered_df.groupby(["SG_UF", "DS_GRAU_INSTRUCAO"], as_index=False)
-    .agg({"SQ_CANDIDATO": "count"})
-)
+    st.title("Análise de Bens Declarados pelos Candidatos - 2024")
 
-# Calcular a porcentagem para cada grau de instrução
-total_candidatos = grouped_df["SQ_CANDIDATO"].sum()
-grouped_df["Porcentagem"] = (
-    grouped_df["SQ_CANDIDATO"] / total_candidatos) * 100
+    # Gráfico 3: Total de bens declarados por partido
+    st.subheader("Total de Bens Declarados por Partido")
+    party_grouped = filtered_data.groupby(
+        "SG_PARTIDO")["VR_BEM_TOTAL"].sum().reset_index()
+    fig = px.bar(
+        party_grouped,
+        x="SG_PARTIDO",
+        y="VR_BEM_TOTAL",
+        title="Total de Bens Declarados por Partido",
+        labels={"SG_PARTIDO": "Partido",
+                "VR_BEM_TOTAL": "Total de Bens Declarados"},
+        template="plotly_white"
+    )
+    st.plotly_chart(fig)
 
-# Criar o gráfico de pizza subdividido por grau de instrução
-fig = px.pie(
-    grouped_df,
-    names="DS_GRAU_INSTRUCAO",
-    values="Porcentagem",
-    color="DS_GRAU_INSTRUCAO",
-    title=f"Distribuição de Grau de Instrução - {selected_states}",
-    template="plotly",
-    labels={"Porcentagem": "% de Candidatos"},
-    hole=0.4  # Gráfico de rosca (opcional)
-)
-st.plotly_chart(fig)
+    # Gráfico 4: Média de bens declarados por estado
+    st.subheader("Média de Bens Declarados por Estado")
+    state_grouped = filtered_data.groupby(
+        "SG_UF")["VR_BEM_TOTAL"].mean().reset_index()
+    fig = px.bar(
+        state_grouped,
+        x="SG_UF",
+        y="VR_BEM_TOTAL",
+        title="Média de Bens Declarados por Estado",
+        labels={"SG_UF": "Estado", "VR_BEM_TOTAL": "Média dos Bens Declarados"},
+        template="plotly_white"
+    )
+    st.plotly_chart(fig)
+
+    # Criar ranking dos mais ricos
+    st.subheader("Ranking dos Candidatos Mais Ricos - 2024")
+    top_n = st.slider("Selecione o número de candidatos para exibir:",
+                      min_value=5, max_value=20, value=10)
+
+    # Ordenar por VR_BEM_TOTAL em ordem decrescente
+    ranking = data.sort_values(by="VR_BEM_TOTAL", ascending=False).head(top_n)
+
+    # Exibir tabela do ranking
+    st.subheader(f"Top {top_n} Candidatos Mais Ricos")
+    st.write(ranking[["NM_CANDIDATO", "SG_UF", "DS_CARGO", "VR_BEM_TOTAL"]])
+
+    # Criar gráfico de barras para os mais ricos
+    st.subheader("Visualização dos Candidatos Mais Ricos")
+    fig = px.bar(
+        ranking,
+        x="NM_CANDIDATO",
+        y="VR_BEM_TOTAL",
+        color="SG_UF",
+        title=f"Top {top_n} Candidatos Mais Ricos",
+        labels={"NM_CANDIDATO": "Candidato",
+                "VR_BEM_TOTAL": "Valor dos Bens Declarados"},
+        template="plotly_white",
+        text_auto=True
+    )
+    st.plotly_chart(fig)
